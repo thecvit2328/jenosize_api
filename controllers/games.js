65abda081acc9bd861ab984c10bb4fe8fcf037ff
watcharapon.kes <watcharapon.kes@gmail.com>
2022-01-20 09:16:21 +0700

    [Game XO] Find best spot with ai player

--- controllers/games.js
+++ controllers/games.js
@@ -33,4 +33,100 @@ const twentyfourGame = async (req, res) => {
   }
 };
 
-module.exports = twentyfourGame;
+const xoGame = {
+  wonCondition: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ],
+  checkWin: (board, player) => {
+    let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+    let gameWon = null;
+
+    for (let [index, win] of xoGame.wonCondition.entries()) {
+      if (win.every((elem) => plays.indexOf(elem) > -1)) {
+        gameWon = { index: index, player: player };
+        break;
+      }
+    }
+
+    return gameWon;
+  },
+  minimax: (newBoard, player) => {
+    const humanPlayer = "x",
+      aiPlayer = "o";
+    var availSpots = newBoard.filter((s) => typeof s == "number");
+
+    if (xoGame.checkWin(newBoard, humanPlayer)) {
+      return { score: -10 };
+    } else if (xoGame.checkWin(newBoard, aiPlayer)) {
+      return { score: 10 };
+    } else if (availSpots.length === 0) {
+      return { score: 0 };
+    }
+    var moves = [];
+    for (var i = 0; i < availSpots.length; i++) {
+      var move = {};
+      move.index = newBoard[availSpots[i]];
+      newBoard[availSpots[i]] = player;
+
+      if (player == aiPlayer) {
+        var result = xoGame.minimax(newBoard, humanPlayer);
+        move.score = result.score;
+      } else {
+        var result = xoGame.minimax(newBoard, aiPlayer);
+        move.score = result.score;
+      }
+
+      newBoard[availSpots[i]] = move.index;
+
+      moves.push(move);
+    }
+
+    var bestMove;
+    if (player === aiPlayer) {
+      var bestScore = -10000;
+      for (var i = 0; i < moves.length; i++) {
+        if (moves[i].score > bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    } else {
+      var bestScore = 10000;
+      for (var i = 0; i < moves.length; i++) {
+        if (moves[i].score < bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    }
+    return moves[bestMove];
+  },
+  bastSpot: async (req, res) => {
+
+    const schema = Joi.object({
+      newBoard: Joi.array().required(),
+      player: Joi.string().min(1).max(9).required(),
+    });
+
+    try {
+      const { newBoard, player } = req.body;
+      console.log(newBoard);
+      console.log(player);
+      const bastSpot = xoGame.minimax(newBoard, player);
+      res.status(200).send(bastSpot);
+    } catch (error) {
+      res.status(400).send(bastSpot);
+    }
+
+    
+  },
+};
+
+module.exports = { twentyfourGame, xoGame };
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
-var express = require("express")
+var express = require("express");
 var router = express.Router();
-var restaurant = require("../controllers/restaurants")
-var twentyfourGame = require("../controllers/games")
+var restaurant = require("../controllers/restaurants");
+var { twentyfourGame, xoGame } = require("../controllers/games");
 
-router.get('/api/search', restaurant.search);
-router.post('/api/game/twentyfour', twentyfourGame);
+router.get("/api/search", restaurant.search);
+router.post("/api/game/twentyfour", twentyfourGame);
+router.post("/api/game/xo", xoGame.bastSpot);
 
 module.exports = router;
